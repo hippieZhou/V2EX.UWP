bc68fb64289742f105c6688127bfbf72711f256d
hippieZhou <hippiezhou@outlook.com>
2017-10-07 16:52:04 +0800

    update

--- V2EX/ViewModels/Home/TabNavigationItem.cs
+++ V2EX/ViewModels/Home/TabNavigationItem.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +19,8 @@ namespace V2EX.ViewModels
         {
             Label = label;
             Name = name;
+
+            //WebUtility.HtmlDecode();
         }
     }
 }
