0e2030e276ae8fcecdef0b14f9174f331c88fe37
hippieZhou <hippiezhou@outlook.com>
2018-04-20 22:37:34 +0800

    change files location

---
+++ V2EX/Models/Topics.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V2EX.Models
+{
+    public class Rootobject
+    {
+        public Topic[] Topics { get; set; }
+    }
+
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Content { get; set; }
+        public string Content_rendered { get; set; }
+        public int Replies { get; set; }
+        public Member Member { get; set; }
+        public Node Node { get; set; }
+        public int Created { get; set; }
+        public int Last_modified { get; set; }
+        public int Last_touched { get; set; }
+    }
+
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Tagline { get; set; }
+        public string Avatar_mini { get; set; }
+        public string Avatar_normal { get; set; }
+        public string Avatar_large { get; set; }
+    }
+
+    public class Node
+    {
+        [PropertyName]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Title_alternative { get; set; }
+        public string Url { get; set; }
+        public int Topics { get; set; }
+        public string Avatar_mini { get; set; }
+        public string Avatar_normal { get; set; }
+        public string Avatar_large { get; set; }
+    }
+}
+
+
---
+++ V2EX/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,27 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V2EX.ViewModels
+{
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        /// <summary>
+        /// Gets the Main property.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+    }
+}
